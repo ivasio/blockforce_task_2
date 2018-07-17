23c046143e2e8be20b9507480781cb5fa4981f88
Ivan Chebotar <van.chebotar@yandex.ru>
2018-07-17 23:19:41 +0300

    Most of the work is done - Added data saving in .xlsx format - Corrected source code errors - After this commit sources are planned to be corrected only in case of bugs

--- main.py
+++ main.py
@@ -1,52 +1,94 @@
 from PyQt5 import QtWidgets, uic
 from crypto import tools as eth
-import time
-
-def showErrorWindow (message):
-	error_dialog = QtWidgets.QMessageBox()
-	error_dialog.setText(message)
-	error_dialog.setWindowTitle("Error")
-	error_dialog.exec_() 
+import openpyxl
 
 
-def displayInfo():
+
+if __name__ == '__main__':
 	
-	if eth.checkInternetConnection() == False : 
-		showErrorWindow ("Интернет-подключение отсутствует!")
-		return False
 
-	walletAddress = dialog.lineEdit.text()
-	check = eth.checkWallet(walletAddress)
-	
-	if check["status"] == "0" :
-		if check["result"] == "Error! Invalid address format" :
-			showErrorWindow("Неверно введен адрес кошелька!")
-		else :
-			print (check.__str__())
-			showErrorWindow("Ошибка!")
-		return False
-	
-	info = eth.getWalletInfo(walletAddress)
-
-	output = "Совершено {} транзакций\n".format(info["number"])
-	if info["sum"] != 0 :
-		output += "Сумма входящих транзакций - {}\n".format(str(info["sum"]))
-		output += "Даты транзакций : \n"
-		for date in info["dates"] :
-			output += "{}\n".format(date)
-
-	dialog.textBrowser.setText(output)
+	def showMessageWindow (message):
+		error_dialog = QtWidgets.QMessageBox()
+		error_dialog.setText(message)
+		error_dialog.setWindowTitle("Error")
+		error_dialog.exec_() 
 
 
 
+	# Отображение информации о кошельке в текстовом окне
+	def displayInfo():
+		
+		if eth.checkInternetConnection() == False : 
+			showMessageWindow ("Интернет-подключение отсутствует!")
+			return False
+
+		walletAddress = window.lineEdit.text()
+
+		if len(walletAddress) < 3 or walletAddress[0:2] != "0x" :
+			showMessageWindow("Неверно введен адрес кошелька!")
+			return False
+
+		check = eth.checkWallet(walletAddress)
+		
+		if check["status"] == "0" :
+			if check["result"] == "Error! Invalid address format" :
+				showMessageWindow("Неверно введен адрес кошелька!")
+			else :
+				showMessageWindow("Ошибка!")
+			return False
+		
+		global walletInfo # Запись в глобальную переменную 
+		walletInfo = eth.getWalletInfo(walletAddress)
+
+		output = "Совершено {} транзакций\n".format(walletInfo["number"])
+		if walletInfo["number"] != 0 :
+			output += "Сумма входящих транзакций - {}\n".format(str(walletInfo["sum"]))
+			output += "Даты транзакций : \n"
+			for date in walletInfo["dates"] :
+				output += "{}\n".format(date)
+
+		window.textBrowser.setText(output)
 
 
-app = QtWidgets.QApplication([])
-dialog = uic.loadUi("window.ui")
+	# Запись в Excel
+	def saveExcel() :
 
-dialog.pushButton.clicked.connect(displayInfo)
+		if walletInfo is None :
+			showMessageWindow("Нет данных для сохранения! Сначала получите \
+				данные с помощью кнопки 'Просмотреть информацию'")
+			return False
+
+		filePath = QtWidgets.QFileDialog.getSaveFileName()[0]
+
+		book = openpyxl.Workbook()
+		sheet = book.active
+		sheet["A1"] = "Совершено транзакций"
+		sheet["B1"] = walletInfo["number"]
+
+		if walletInfo["number"] != 0 :
+			sheet["A2"] = "Сумма входящих транзакций"
+			sheet["B2"] = walletInfo["sum"]
+			sheet["A3"] = "Даты транзакций"
+			counter = 4
+			for date in walletInfo["dates"] :
+				sheet["A" + str(counter)] = date
+				counter += 1
+
+		book.save(filePath)
+		showMessageWindow("Данные сохранены!")
 
 
-dialog.show()
-app.exec()
+
+	walletInfo = None
+
+	app = QtWidgets.QApplication([])
+	window = uic.loadUi("window.ui")
+
+	# Обработчики кнопок
+	window.pushButton.clicked.connect(displayInfo)
+	window.saveButton.clicked.connect(saveExcel)
+
+	# Запуск основного цикла приложения
+	window.show()
+	app.exec()
 
