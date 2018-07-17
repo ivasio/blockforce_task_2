6ce96676aeb6799adca1e9926523160ad6cb5672
Ivan Chebotar <van.chebotar@yandex.ru>
2018-07-17 21:17:15 +0300

    Added checks and error handling - Lack of connection and wrong  wallet adress cases are taken into consideration - For now program outputs wallet balance

--- crypto/tools.py
+++ crypto/tools.py
@@ -7,10 +7,35 @@ CONFIG = configparser.ConfigParser()
 CONFIG.read('crypto\provider.conf')
 
 
-def getWalletsBalance(walletAddress) :
+def checkInternetConnection() :
+	try : 
+		response = requests.get("https://google.com")
+		return True
+	except :
+		return False
+
+
+def checkProvider(currency) :
+
+	url = CONFIG[currency]["Test_Provider"].format(key = CONFIG[currency]["API_Key"])
+	response = requests.get(url).json()
+
+	return bool(int(response["status"]))
+	
+
+def checkWallet(walletAddress) :
 
 	url = CONFIG["Ethereum"]["Provider"].format(key = CONFIG["Ethereum"]["API_Key"], 
 		address = walletAddress)
 	
-	response = requests.get(url)
+	response = requests.get(url).json()
+	return response
+
+
+def getWalletBalance(walletAddress) :
+
+	url = CONFIG["Ethereum"]["Provider"].format(key = CONFIG["Ethereum"]["API_Key"], 
+		address = walletAddress)
+	
+	response = requests.get(url).json()
 	return response.json()
--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 from PyQt5 import QtWidgets, uic
 from crypto import tools as eth
+import time
+
+def showErrorWindow (message):
+	error_dialog = QtWidgets.QMessageBox()
+	error_dialog.setText(message)
+	error_dialog.setWindowTitle("Error")
+	error_dialog.exec_() 
 
 
 def displayInfo():
+	
+	if eth.checkInternetConnection() == False : 
+		showErrorWindow ("Интернет-подключение отсутствует!")
+		return False
+
 	walletAddress = dialog.lineEdit.text()
-	info = eth.getWalletsBalance(walletAddress)
+	check = eth.checkWallet(walletAddress)
+	
+	if check.status == "0" :
+		if check.result == "Error! Invalid address format" :
+			showErrorWindow ("Неверно введен адрес кошелька!")
+		else :
+			showErrorWindow ("Ошибка!")
+		return False
+	
+	info = eth.getWalletBalance(walletAddress)
 	dialog.textBrowser.setText(info["result"])
+
+
+
 
 
 app = QtWidgets.QApplication([])
@@ -13,5 +37,8 @@ dialog = uic.loadUi("window.ui")
 
 dialog.pushButton.clicked.connect(displayInfo)
 
+
 dialog.show()
+print ("shit")
 app.exec()
+
